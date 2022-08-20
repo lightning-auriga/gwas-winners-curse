3f182f37c4c1547bdcdf252e2fb9d1d9c51b204f
cpalmer718 <palmer.cam@gmail.com>
2022-08-20 15:48:23 -0400

    test: add stability tests for primary winners curse correction function
    regrettably, there aren't exactly ground truth options available for this at the moment, so these
    tests consistency (within 10%, which is admittedly high) between this implementation and the last.
    the supplemental files are ones I generated years ago, for
    https://doi.org/10.1371/journal.pgen.1006916

--- tests/testthat/test_correct_winners_curse.R
+++ tests/testthat/test_correct_winners_curse.R
@@ -1,3 +1,59 @@
+test.file.codes <- list.files(".", "*test_input.tsv", recursive = TRUE, full.names = TRUE)
+test.file.codes <- gsub("_test_input.tsv", "", sapply(strsplit(test.file.codes, "/"), function(i) {
+  i[[4]]
+}))
+## this is admittedly lenient. the largest relative deviation is just over 5%. I really
+## don't know whether this should be considered truly problematic or not. it looks like
+## just general precision issues, but who can say.
+permitted.rel.tolerance <- 0.1
+
 test_that("correct.winners.curse functions as expected", {
-  skip("test not implemented")
+  for (file.code in test.file.codes) {
+    test.input <- file.path(
+      "testthat_files", "correct_winners_curse",
+      paste(file.code, "_test_input.tsv", sep = "")
+    )
+    test.expected <- file.path(
+      "testthat_files", "correct_winners_curse",
+      paste(file.code, "_test_expected.tsv", sep = "")
+    )
+    test.expected <- read.table(test.expected, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+    test.output <- tempfile(paste("correct.winners.curse.", file.code, sep = ""), fileext = ".tsv")
+    ## suppress warnings here, as we're not concerned at the moment with whether the
+    ## p-value threshold needs dynamic adjustment
+    suppressWarnings(correct.winners.curse(test.input, test.output, NA, NA, TRUE, "\t"))
+    test.observed <- read.table(test.output, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+    ## there are expected deviations in extreme precision values for these methods,
+    ## so we're really only looking for very general concordance, but within 10% or so
+    expected.beta <- test.expected[, "old_corrected_beta"]
+    observed.beta <- test.observed[, "debiased.beta.mle"]
+    expected.l95 <- test.expected[, "old_l95"]
+    observed.l95 <- test.observed[, "l95.mle"]
+    expected.u95 <- test.expected[, "old_u95"]
+    observed.u95 <- test.observed[, "u95.mle"]
+    if (length(which(abs(observed.beta - expected.beta) / expected.beta >= permitted.rel.tolerance)) > 0) {
+      print(paste(file.code, "beta", sep = " "))
+      print(cbind(
+        observed.beta,
+        expected.beta
+      )[abs(observed.beta - expected.beta) / expected.beta >= permitted.rel.tolerance, ])
+    }
+    if (length(which(abs(observed.l95 - expected.l95) / expected.l95 >= permitted.rel.tolerance)) > 0) {
+      print(paste(file.code, "l95", sep = " "))
+      print(cbind(
+        observed.l95,
+        expected.l95
+      )[abs(observed.l95 - expected.l95) / expected.l95 >= permitted.rel.tolerance, ])
+    }
+    if (length(which(abs(observed.u95 - expected.u95) / expected.u95 >= permitted.rel.tolerance)) > 0) {
+      print(paste(file.code, "u95", sep = " "))
+      print(cbind(
+        observed.u95,
+        expected.u95
+      )[abs(observed.u95 - expected.u95) / expected.u95 >= permitted.rel.tolerance, ])
+    }
+    expect_true(all(abs(observed.beta - expected.beta) / expected.beta < permitted.rel.tolerance))
+    expect_true(all(abs(observed.l95 - expected.l95) / expected.l95 < permitted.rel.tolerance))
+    expect_true(all(abs(observed.u95 - expected.u95) / expected.u95 < permitted.rel.tolerance))
+  }
 })
