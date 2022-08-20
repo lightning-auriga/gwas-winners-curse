fe4d446cded225488315a142c89fc5f04237e9b8
Lightning Auriga <lightning.auriga@gmail.com>
2022-08-20 12:02:39 -0400

    feat: add primary logic for debiasing and MSE correction of linear regression data
    this ports in the remaining primary logic from the original implementation. several alternate run
    modes are excluded, as they had been initially intended for debugging; they may be added back. this
    reimplementation once again removes a large amount of logic for GLM support outside of linear
    regression. this also revealed some hackjob nonsense that I had to add at the end to override MSE
    correction, as it never seemed to work right. this will need to be addressed now.

---
+++ R/compute_mse.R
@@ -0,0 +1,45 @@
+#' @title
+#' Compute MSE adjustment for beta correction.
+#'
+#' @description
+#' Computes weighted average of biased and strictly debiased
+#' regression coefficient to report as intermediate result.
+#'
+#' @details
+#' Note that this was apparently completely disabled in the original
+#' implementation, such that the output reflected the MLE correction only.
+#'
+#' @param biased.beta Numeric input regression coefficient from discovery.
+#' @param biased.se Numeric input coefficient standard error from discovery.
+#' @param debiased.beta Numeric MLE adjusted regression coefficient.
+#' @return Numeric weighted average coefficient reflecting MSE adjustment.
+compute.beta.mse <- function(biased.beta,
+                             biased.se,
+                             debiased.beta) {
+  k <- biased.se^2 / (biased.se^2 + (biased.beta - debiased.beta)^2)
+  k * biased.beta + (1 - k) * debiased.beta
+}
+
+#' @title
+#' Compute MSE adjustment for confidence interval correction.
+#'
+#' @description
+#' Computes weighted average of biased and strictly debiased
+#' regression coefficient confidence interval to report as intermediate result.
+#'
+#' @details
+#' Note that this was apparently completely disabled in the original
+#' implementation, such that the output reflected the MLE correction only.
+#'
+#' @param biased.beta Numeric input regression coefficient from discovery.
+#' @param biased.se Numeric input coefficient standard error from discovery.
+#' @param existing.ci Numeric MLE adjusted confidence interval.
+#' @return Numeric weighted average confidence interval reflecting MSE adjustment.
+compute.ci.mse <- function(biased.beta,
+                           biased.se,
+                           existing.ci,
+                           p.thresh) {
+  biased.ci <- calculate.ci(biased.beta, biased.se, p.thresh)
+  k <- biased.se^2 / (biased.se^2 + (biased.ci - existing.ci)^2)
+  k * biased.ci + (1 - k) * existing.ci
+}
--- R/correct_winners_curse.R
+++ R/correct_winners_curse.R
@@ -1,5 +1,5 @@
 #' @title
-#' Correct Winner's Curse in GWAS association data
+#' Correct Winner's Curse in GWAS association data.
 #'
 #' @description
 #' TBD
@@ -12,28 +12,83 @@
 #' @param trait.mean Numeric mean of trait used for discovery.
 #' If NA, the function will expect mean values to be specified in
 #' each row of the input data file.
-#' @param stderr.precision Numeric requested precision of standard
-#' error calculation.
 #' @param discovery.threshold Numeric threshold on p-value used
-#' in discovery phase to select variants for replication.
+#' in discovery phase to select variants for replication. If NA,
+#' the function will expect threshold values to be specified in
+#' each row of the input data file.
 #' @param header Logical indicating whether input data file
 #' has a header.
-#' @param invariant.stderr Logical indicating whether to use
-#' the simplifying assumption that the standard error does not
-#' change along with shrinking effect estimate.
+#' @param sep Character vector indicating delimiter in input data.
 #' @return NULL
 #' @export
 correct.winners.curse <- function(input.file,
                                   output.file,
-                                  regression.type = c("linear", "logistic", "poisson"),
-                                  trait.mean = NA,
-                                  stderr.precision = NA,
+                                  trait.mean = 0,
                                   discovery.threshold = 5e-8,
                                   header = TRUE,
-                                  invariant.stderr = FALSE) {
+                                  sep = sep) {
   stopifnot(length(input.file) == 1)
   stopifnot(is.character(input.file))
+  stopifnot(file.exists(input.file))
   stopifnot(length(output.file) == 1)
   stopifnot(is.character(output.file))
-  regression.type <- match.arg(regression.type)
+  stopifnot(length(trait.mean) == 1)
+  stopifnot(is.numeric(trait.mean) || is.na(trait.mean))
+  stopifnot(length(discovery.threshold) == 1)
+  stopifnot(is.numeric(discovery.threshold) || is.na(discovery.threshold))
+
+  ## load input data
+  data <- read.table(input.file, header = header, sep = sep, stringsAsFactors = FALSE)
+  stopifnot(ncol(data) %in% c(8, 10))
+  if (ncol(data) == 10) {
+    colnames(data) <- c(
+      "discovery.beta", "discovery.se", "discovery.n", "discovery.freq",
+      "trait.mean", "discovery.threshold",
+      "replication.beta", "replication.se", "replication.n", "replication.freq"
+    )
+  } else {
+    colnames(data) <- c(
+      "discovery.beta", "discovery.se", "discovery.n", "discovery.freq",
+      "replication.beta", "replication.se", "replication.n", "replication.freq"
+    )
+    data$trait.mean <- trait.mean
+    data$discovery.threshold <- discovery.threshold
+  }
+
+  ## debias beta
+  data$debiased.beta <- sapply(seq_len(nrow(data)), function(i) {
+    debias.beta(
+      data[i, "discovery.beta"],
+      data[i, "discovery.se"],
+      data[i, "discovery.freq"],
+      data[i, "trait.mean"],
+      data[i, "discovery.threshold"]
+    )
+  })
+  ## compute 95% confidence interval around debiased beta
+  data$l95 <- calculate.ci(data[, "debiased.beta"], data[, "discovery.se"], 0.025)
+  data$u95 <- calculate.ci(data[, "debiased.beta"], data[, "discovery.se"], 0.975)
+  ## compute MSE variants of debiased results
+  data$debiased.beta.mse <- compute.beta.mse(
+    data[, "discovery.beta"],
+    data[, "discovery.se"],
+    data[, "debiased.beta"]
+  )
+  data$l95.mse <- compute.ci.mse(
+    data[, "discovery.beta"],
+    data[, "discovery.se"],
+    data[, "l95"],
+    0.025
+  )
+  data$u95.mse <- compute.ci.mse(
+    data[, "discovery.beta"],
+    data[, "discovery.se"],
+    data[, "u95"],
+    0.975
+  )
+  ## emit results
+  write.table(data, output.file,
+    row.names = FALSE, col.names = TRUE, quote = FALSE,
+    sep = ifelse(grepl("\\.csv$", output.file), ",", "\t")
+  )
 }
