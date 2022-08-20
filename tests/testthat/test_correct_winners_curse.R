dade5c3d58a040b7c94269a57f87bf5d4f203efa
cpalmer718 <palmer.cam@gmail.com>
2022-08-20 15:57:22 -0400

    test: add test for 8 column input, no header, comma delimited I/O

--- tests/testthat/test_correct_winners_curse.R
+++ tests/testthat/test_correct_winners_curse.R
@@ -57,3 +57,42 @@ test_that("correct.winners.curse functions as expected", {
     expect_true(all(abs(observed.u95 - expected.u95) / expected.u95 < permitted.rel.tolerance))
   }
 })
+
+test_that("correct.winners.curse can deal with files that don't contain per-row means and p thresholds", {
+  test.input <- file.path("testthat_files", "correct_winners_curse", "24963161_no_mean_p.csv")
+  test.expected <- file.path("testthat_files", "correct_winners_curse", "24963161_test_expected.tsv")
+  test.expected <- read.table(test.expected, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+  test.output <- tempfile("correct_winners_curse_truncated", fileext = ".csv")
+  suppressWarnings(correct.winners.curse(test.input, test.output, 0, 5e-6, FALSE, ","))
+  test.observed <- read.table(test.output, header = TRUE, stringsAsFactors = FALSE, sep = ",")
+  expected.beta <- test.expected[, "old_corrected_beta"]
+  observed.beta <- test.observed[, "debiased.beta.mle"]
+  expected.l95 <- test.expected[, "old_l95"]
+  observed.l95 <- test.observed[, "l95.mle"]
+  expected.u95 <- test.expected[, "old_u95"]
+  observed.u95 <- test.observed[, "u95.mle"]
+  if (length(which(abs(observed.beta - expected.beta) / expected.beta >= permitted.rel.tolerance)) > 0) {
+    print("beta")
+    print(cbind(
+      observed.beta,
+      expected.beta
+    )[abs(observed.beta - expected.beta) / expected.beta >= permitted.rel.tolerance, ])
+  }
+  if (length(which(abs(observed.l95 - expected.l95) / expected.l95 >= permitted.rel.tolerance)) > 0) {
+    print("l95")
+    print(cbind(
+      observed.l95,
+      expected.l95
+    )[abs(observed.l95 - expected.l95) / expected.l95 >= permitted.rel.tolerance, ])
+  }
+  if (length(which(abs(observed.u95 - expected.u95) / expected.u95 >= permitted.rel.tolerance)) > 0) {
+    print("u95")
+    print(cbind(
+      observed.u95,
+      expected.u95
+    )[abs(observed.u95 - expected.u95) / expected.u95 >= permitted.rel.tolerance, ])
+  }
+  expect_true(all(abs(observed.beta - expected.beta) / expected.beta < permitted.rel.tolerance))
+  expect_true(all(abs(observed.l95 - expected.l95) / expected.l95 < permitted.rel.tolerance))
+  expect_true(all(abs(observed.u95 - expected.u95) / expected.u95 < permitted.rel.tolerance))
+})
