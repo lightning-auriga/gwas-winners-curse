9d2cf4812f60b65174db558f8b2ff00afb0b1728
cpalmer718 <palmer.cam@gmail.com>
2022-08-21 16:44:13 -0400

    feat: add initial, untested implementation of replication functions and Poisson binomial test
    PoissonBinomial package is now a hard requirement for installation. Replication functions assume
    linearity and unit variance; this may yet be relaxed. Unit tests to come.

---
+++ R/poisson_binomial_test.R
@@ -0,0 +1,61 @@
+#' @title
+#' Performs an approximate test of a simple null hypothesis
+#' about the probability of success in a Poisson binomial
+#' series.
+#'
+#' @description
+#' This function extends stats::binom.test for the Poisson
+#' binomial distribution. Most of the original binom.test
+#' functionality is removed in favor of a streamlined
+#' calculation of the p-value itself.
+#'
+#' @details
+#' To be entirely frank, I don't have a great deal of confidence
+#' in whether this thing works or not. It seems like a clever trick
+#' to me, but then, it's entirely possible it isn't implemented
+#' in the source package itself for a reason.
+#'
+#' @param x Numeric number of success counts.
+#' @param n Numeric number of trials.
+#' @param p Numeric sequence of probabilities for each trial in turn.
+#' @param alternative Character, one of "two.sided", "less", or "greater."
+#' Analogous to the same parameter in binom.test.
+#' @return Numeric probability of the alternative hypothesis
+#' given the input parameters.
+#' @export
+#' @examples
+#' x <- 20
+#' n <- 30
+#' p <- seq(0, 1, length.out = 10)
+#' poisson.binom.test(x, n, p, "greater")
+poisson.binom.test <- function(x, n, p = 0.5, alternative = c("two.sided", "less", "greater")) {
+  stopifnot(length(x) == 1)
+  stopifnot(is.numeric(x) || is.integer(x))
+  stopifnot(length(n) == 1)
+  stopifnot(is.numeric(n) || is.integer(n))
+  stopifnot(x <= n, x >= 0)
+  alternative <- match.arg(alternative)
+  p.value <- switch(alternative,
+    less = PoissonBinomial::ppbinom(x, n, p),
+    greater = PoissonBinomial::ppbinom(x - 1, n, p, lower.tail = FALSE),
+    two.sided = {
+      rel.err <- 1 + 1e-07
+      d <- dpbinom(x, n, p)
+      m <- n * p
+      if (x == m) {
+        1
+      } else if (x < m) {
+        i <- seq.int(from = ceiling(m), to = n)
+        y <- sum(PoissonBinomial::dpbinom(i, n, p) <= d * rel.err)
+        PoissonBinomial::ppbinom(x, n, p) +
+          PoissonBinomial::ppbinom(n - y, n, p, lower.tail = FALSE)
+      } else {
+        i <- seq.int(from = 0, to = floor(m))
+        y <- sum(PoissonBinomial::dbinom(i, n, p) <= d * rel.err)
+        PoissonBinomial::ppbinom(y - 1, n, p) +
+          PoissonBinomial::ppbinom(x - 1, n, p, lower.tail = FALSE)
+      }
+    }
+  )
+  p.value
+}
